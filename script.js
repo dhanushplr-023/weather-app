839ea040cb5c0ad125a4ff9f8f85bfbbd85bbdfd
PYRO-DHANUSH <dhanushdhanush230904@gmail.com>
2026-06-17 23:29:19 +0530

    outlook with certain features Modern dark dashboard
    ✅ Weather summary card
    
    ✅ Forecast cards
    
    ✅ Rain chart section
    
    ✅ Temperature graph
    
    ✅ Nearby cities panel
    
    ✅ Mobile responsive layout

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Weather App Started");
+const ctx = document.getElementById('tempChart');
+
+new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Mon','Tue','Wed','Thu','Fri'],
+        datasets: [{
+            label: 'Temperature',
+            data: [22,24,23,25,27]
+        }]
+    }
+});
